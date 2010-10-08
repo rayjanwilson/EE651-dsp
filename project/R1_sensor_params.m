dc27adb00fa6fd5b98be977d2cc1bf8a50dd0884
Rayjan Wilson <rayjan.wilson@gmail.com>
2010-10-08 15:43:00 -0800

    R1_sensor_params now instantiates a Metadata object and returns it to the rest of the program to use for the image paramters

--- project/R1_sensor_params.m
+++ project/R1_sensor_params.m
@@ -1,13 +1,19 @@
+function meta = R1_sensor_params()
+
 % ------------------------------------------------------------------------
 % Radarsat Sensor Parameters
 % ------------------------------------------------------------------------
-wc = 5.3e9;                         % carrier frequency [Hz]
-w0 = 1.748e7;                       % chirp bandwidth [Hz]
-Tp = 4.2e-5;                        % chirp pulse duration [s]
-alpha = -4.1619e11;                 % chirp rate [Hz/s]
-prf = 1295.923;                     % pulse repetition frequency [Hz]
-f_rs = 1.84669e7;                   % range sampling frequency
-v = 7540.357;                       % platform velocity [m/s]
-lambda = 0.05656;            % carrier freq wavelength
-importfile('RSAT_S1_antenna.gain');
+
+meta = Metadata;    %create the metadata object
+
+meta.wc = 5.3e9;                         % carrier frequency [Hz]
+meta.w0 = 1.748e7;                       % chirp bandwidth [Hz]
+meta.Tp = 4.2e-5;                        % chirp pulse duration [s]
+meta.alpha = -4.1619e11;                 % chirp rate [Hz/s]
+meta.prf = 1295.923;                     % pulse repetition frequency [Hz]
+meta.f_rs = 1.84669e7;                   % range sampling frequency
+meta.v = 7540.357;                       % platform velocity [m/s]
+meta.lambda = 0.05656;            % carrier freq wavelength
+%importfile('RSAT_S1_antenna.gain');
 % ------------------------------------------------------------------------
+end
